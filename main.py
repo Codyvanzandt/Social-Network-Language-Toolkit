1ff90eec711b6ae797d8a1c10517f5434bf6fee6
Cody VanZandt <codyvanzandt@wustl.edu>
2020-05-22 21:43:19 +0000

    prototype enter exit notation

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from src.social_network.social_network import SocialNetwork
 from src.edge_notations.character_mapping_notation import CharacterMappingNotation
+from src.edge_notations.enter_exit_notation import EnterExitNotation
 from pprint import pprint
 
 s = SocialNetwork("examples/fake_play.yaml")
-pprint(s.data["edges"])
-e = CharacterMappingNotation(s.data["edges"])
+e = EnterExitNotation(s.data["edges"], directed=False)
 pprint(e.edges)

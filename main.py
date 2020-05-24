fa0f7e199b159d66563353cba2ea09236c6e3e7f
Cody VanZandt <codyvanzandt@wustl.edu>
2020-05-24 18:02:41 +0000

    Reverting back to YAML, ugh!

--- main.py
+++ main.py
@@ -1,33 +1,7 @@
 from src.social_network import SocialNetwork
-from src.edge_notations.character_mapping_notation import CharacterMappingNotation
 from pprint import pprint
 import toml
-import networkx
+import yaml
 
-f = """
-[play]
-title = "Some Title"
-author = "Some Author"
-
-[characters]
-Isabella = { gender = "female", archetype = "innamorati" }
-Flavio = { gender = "male", archetype = "innamorati" }
-Pantalone = { gender = "male", archetype = "vecchi" }
-
-[network]
-weighted = true
-directed = true
-
-[edges.act1.scene1]
-Isabella.Pantalone = {type = "hit", weight = 1}
-Isabella.Flavio = {type = "kissed", weight = 4}
-Flavio.Pantalone = {type = "hit", weight = 3}
-
-[edges.act2.scene1]
-Isabella.Pantalone = {type = "hit", weight = 1}
-Isabella.Flavio = {type = "kissed", weight = 4}
-Flavio.Pantalone = {type = "hit", weight = 3}
-"""
-
-s = SocialNetwork(f)
-pprint(list(s.get_edge_list()))
+with open("examples/fake_play.yaml","r") as fake_play:
+    pprint(yaml.load( fake_play, Loader=yaml.FullLoader))

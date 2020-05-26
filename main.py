4953121e75d75d25ae14b8dd8d1129f55bf5fd08
Cody VanZandt <codyvanzandt@wustl.edu>
2020-05-26 20:43:02 +0000

    reorganizing sdl_tools

--- main.py
+++ main.py
@@ -5,8 +5,3 @@ import yaml
 
 pprint(load_sdl_file("examples/fake_play"))
 
-# #
-# doc = parse_sdl_file("examples/fake_play")
-# play = doc.section('play').elements()[0]
-# print(play)
-# print(dir(play))

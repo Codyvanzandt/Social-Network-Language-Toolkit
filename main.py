8bbf4376bc364b9198d2131b521eabadffc2eacb
Cody VanZandt <codyvanzandt@wustl.edu>
2020-05-27 15:21:35 +0000

    Got string converter working

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from src.drama_network import DramaNetwork
 from src.converters.edge_list_converter import convert_to_edge_list
 from src.converters.networkx_converter import convert_to_networkx
+from src.converters.string_converter import convert_to_string
 from pprint import pprint
 import toml
 import yaml
 
 dn = DramaNetwork("examples/fake_play")
-for edge in dn.to_edge_list(play_data=True, division_data=True):
-    pprint(edge)
+print(convert_to_string(dn))
---
+++ src/converters/string_converter.py
@@ -0,0 +1,59 @@
+import yaml
+
+
+def convert_to_string(social_network):
+    return (
+        convert_section_to_string(social_network, "play")
+        + convert_section_to_string(social_network, "characters")
+        + convert_section_to_string(social_network, "network")
+        + convert_edges_section_to_string(social_network)
+    )
+
+
+def convert_section_to_string(social_network, section_name):
+    section_data = social_network.data.get(section_name, dict())
+    return (
+        convert_section_name_to_string(section_name)
+        + convert_section_data_to_string(section_data)
+        + "\n"
+    )
+
+
+def convert_section_name_to_string(section_name, section_level=1):
+    return f"{'#'*section_level} {section_name}\n"
+
+
+def convert_section_data_to_string(section_data):
+    return "".join(
+        f"{data_key} : {convert_yaml_data_to_string(yaml_data)}"
+        for data_key, yaml_data in section_data.items()
+    )
+
+
+def convert_yaml_data_to_string(yaml_data):
+    return yaml.dump(yaml_data, default_flow_style=True).replace("...\n", "")
+
+
+def convert_edges_section_to_string(social_network):
+    edges_data = social_network.data.get("edges", dict())
+    return (
+        convert_section_name_to_string("edges")
+        + convert_edges_data_to_string(edges_data)
+    ) 
+
+def convert_edges_data_to_string(edges_data, section_level=1):
+    try:
+        return convert_edges_list_to_string(edges_data)
+    except ValueError:
+        return "\n".join(
+            convert_section_name_to_string(sub_section, section_level + 1)
+            + convert_edges_data_to_string(sub_section_data, section_level + 1)
+            for sub_section, sub_section_data in edges_data.items()
+        )
+
+
+def convert_edges_list_to_string(edges_list):
+    return "".join(
+        f"{source_character}.{target_character} : {convert_yaml_data_to_string(edge_data)}"
+        for source_character, target_character, edge_data in edges_list
+    )

c01262a3393a15ef374eff7583c090cf21c3d0c3
Cody VanZandt <codyvanzandt@wustl.edu>
2020-06-07 21:12:02 +0000

    Adding tests for file converter; converter api in the drama network itself

--- tests/test_drama_network.py
+++ tests/test_drama_network.py
@@ -1,6 +1,9 @@
 import pytest
 from pprint import pformat
 from src.drama_network import DramaNetwork
+from src.converters.string_converter import convert_to_string
+from src.converters.edge_list_converter import convert_to_edge_list
+from src.converters.sdl_file_converter import convert_to_file
 
 
 def test_init(fake_drama_network):
@@ -25,15 +28,24 @@ def test__load_sdl_data(
 
 
 def test_to_string(fake_drama_network):
-    pass
+    assert fake_drama_network.to_string() == convert_to_string(fake_drama_network)
 
 
-def test_to_file(fake_drama_network):
-    pass
+def test_to_file(fake_drama_network, tmp_path):
+    from_to_file = tmp_path / "from_file.sdl"
+    from_convert_to_file = tmp_path / "from_convert_to_file.sdl"
+    fake_drama_network.to_file(path=from_to_file)
+    convert_to_file(fake_drama_network, from_convert_to_file)
+    assert (
+        DramaNetwork(from_to_file.absolute()).data
+        == DramaNetwork(from_convert_to_file.absolute()).data
+    )
 
 
 def test_to_edge_list(fake_drama_network):
-    pass
+    assert fake_drama_network.to_edge_list() == list(
+        convert_to_edge_list(fake_drama_network)
+    )
 
 
 def test_to_networkx(fake_drama_network):

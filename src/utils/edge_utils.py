754f6a6e5d34a81a4e573b2b877753d9dff83e55
Cody VanZandt <codyvanzandt@wustl.edu>
2020-06-17 01:25:09 +0000

    Testing walk_nested_edges

--- src/utils/edge_utils.py
+++ src/utils/edge_utils.py
@@ -1,9 +1,23 @@
+import itertools
+
+
 def walk_nested_edges(edge_data, func=None):
     func = func if func is not None else lambda x: x
     if isinstance(edge_data, list):
         return func(edge_data)
     elif isinstance(edge_data, dict):
         return {
-            section_key: walk_nested_edges(section_value)
+            section_key: walk_nested_edges(section_value, func=func)
             for section_key, section_value in edge_data.items()
         }
+
+
+def flatten_nested_edges(edge_data):
+    if isinstance(edge_data, list):
+        return edge_data
+    else:
+        return list(
+            itertools.chain.from_iterable(
+                flatten_nested_edges(subsection) for _, subsection in edge_data.items()
+            )
+        )
--- tests/utils/test_edge_utils.py
+++ tests/utils/test_edge_utils.py
@@ -1,8 +1,40 @@
 import pytest
-from src.utils.edge_utils import walk_nested_edges
+from src.utils.edge_utils import walk_nested_edges, flatten_nested_edges
 from pprint import pprint
 
+EDGE_DATA = {
+    "act1": {
+        "scene1": [
+            ("A", "B", {"type": 1}),
+            ("B", "C", {"type": 1}),
+            ("D", "E", {"type": 1}),
+        ],
+        "scene2": [("E", "F", {"type": 1})],
+    }
+}
 
-def test_walk_nested_edges(fake_play_data):
-    edge_data = fake_play_data["edges"]
-    pprint(walk_nested_edges(edge_data))
+
+def test_walk_nested_edges():
+
+    assert walk_nested_edges(EDGE_DATA, lambda x: x) == EDGE_DATA
+
+    empty_edge_data = {
+        "act1": {
+            "scene1": [("A", "B", {}), ("B", "C", {}), ("D", "E", {})],
+            "scene2": [("E", "F", {})],
+        }
+    }
+
+    def remove_data(edge_list):
+        return [(s, t, {}) for s, t, _ in edge_list]
+
+    assert walk_nested_edges(EDGE_DATA, remove_data) == empty_edge_data
+
+
+def test_flatten_nested_edges():
+    expected_edges = [
+        ("A", "B", {"type": 1}),
+        ("B", "C", {"type": 1}),
+        ("D", "E", {"type": 1}),
+        ("E", "F", {"type": 1}),
+    ]

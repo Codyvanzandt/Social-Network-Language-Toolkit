00e42e26232fca50337ae328742f1838f5d8ad86
Cody VanZandt <codyvanzandt@wustl.edu>
2020-06-29 04:42:56 +0000

    removing main

--- main.py
+++
@@ -1,7 +0,0 @@
-from src.sdl_tools.sdl_document import SDLDocument
-from src.drama_network import DramaNetwork
-from src.converters.sdl_converter import NetworkxToSDLConverter
-from src.converters.drama_network_converter import NXToDramaNetwork
-
-a = DramaNetwork("examples/fake_play")
-print(a.edges(data=True))

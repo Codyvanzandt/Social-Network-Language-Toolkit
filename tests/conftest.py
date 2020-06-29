1ecca5969d953d9754e83e807bb912fc7f0797d6
Cody VanZandt <codyvanzandt@wustl.edu>
2020-06-29 23:10:40 +0000

    Cleaning up dead weight from conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,26 +9,6 @@ def fake_drama_network(fake_play_string):
     return DramaNetwork(fake_play_string, directed=True)
 
 
-# dnl Document
-@pytest.fixture
-def fake_dnl_document(fake_play_string):
-    return DNLDocument(fake_play_string)
-
-
-# DOCUMENT SECTIONS
-@pytest.fixture
-def section():
-    def _section(section_name, field_data):
-        formatted_fields = "\n".join(field for field, *_ in field_data)
-        section_string = f"""
-        # {section_name}
-        {formatted_fields}
-        """
-        return enolib.parse(section_string)
-
-    return _section
-
-
 # PLAY STRING, PLAY FILE, PLAY DATA, and PLAY DOCUMENT
 
 
@@ -37,11 +17,6 @@ def fake_play_file(fake_play_string, tmp_path):
     fake_file = tmp_path / "fake_file.dnl"
     fake_file.write_text(fake_play_string)
     return fake_file
-
-
-@pytest.fixture
-def fake_play_dnl_doc(fake_play_string):
-    return enolib.parse(fake_play_string)
 
 
 @pytest.fixture
@@ -76,46 +51,6 @@ Isabella.Flavio : {}
 Pantalone.Flavio : {}
 Flavio.Pantalone : {'type': 'hit', 'weight': 1}
 """
-
-
-@pytest.fixture
-def fake_play_data():
-    return {
-        "play": {
-            "title": "a title",
-            "author": "an author",
-            "boolean": True,
-            "integer": 42,
-            "float": 42.0,
-            "array": [42, 42, 42],
-            "nested": {"nested": "nested"},
-        },
-        "characters": {
-            "Flavio": {"archetype": "innamorati", "gender": "male",},
-            "Isabella": {"archetype": "innamorati", "gender": "female"},
-            "Pantalone": {"archetype": "vecchi", "gender": "male"},
-        },
-        "edges": {
-            "act1": {
-                "scene1": [
-                    ("Isabella", "Flavio", {"type": "kissed", "weight": 1,},),
-                    ("Isabella", "Flavio", {"type": "kissed", "weight": 3,},),
-                    ("Flavio", "Isabella", {"type": "kissed", "weight": 5,},),
-                    ("Isabella", "Flavio", {"type": "hit", "weight": 7,},),
-                ],
-                "scene2": [
-                    ("Isabella", "Pantalone", {"type": "hit", "weight": 1,},),
-                    ("Isabella", "Flavio", {},),
-                ],
-            },
-            "act2": {
-                "scene1": [
-                    ("Pantalone", "Flavio", {},),
-                    ("Flavio", "Pantalone", {"type": "hit", "weight": 1,},),
-                ]
-            },
-        },
-    }
 
 
 @pytest.fixture

f8d161f359d4a917295af277c615ee8c8d00c7df
Cody VanZandt <codyvanzandt@wustl.edu>
2020-06-29 23:22:07 +0000

    Working on edge_utils

--- tests/utils/test_edge_utils.py
+++ tests/utils/test_edge_utils.py
@@ -1,13 +1,38 @@
 import pytest
-from pprint import pprint
+from src.utils.edge_utils import combine_all_edges, edges_equal, combine_edges
 
-EDGE_DATA = {
-    "act1": {
-        "scene1": [
-            ("A", "B", {"type": 1}),
-            ("B", "C", {"type": 1}),
-            ("C", "D", {"type": 1}),
-        ],
-        "scene2": [("D", "E", {"type": 1})],
-    }
-}
+EDGES = [
+    ("A", "B", {"type": 1, "divisions": (1, 2)}),
+    ("A", "B", {"type": 1, "divisions": (1, 2)}),
+    ("A", "C", {"type": 1, "divisions": (1, 2)}),
+    ("A", "B", {"type": 2, "divisions": (1, 2)}),
+    ("A", "B", {"type": 1, "divisions": (3, 4)}),
+    ("A", "B", {}),
+]
+
+
+def test_combine_all_edges():
+    expected_edges = [
+        ("A", "B", {"weight": 2, "type": 1, "divisions": (1, 2)},)
+    ] + EDGES[1::]
+    assert combine_all_edges(EDGES, edges_equal, combine_edges) == expected_edges
+
+
+def test_edges_equal():
+    assert edges_equal(EDGES[0], EDGES[1])
+    assert not edges_equal(EDGES[0], EDGES[2])
+    assert not edges_equal(EDGES[0], EDGES[3])
+    assert not edges_equal(EDGES[0], EDGES[4])
+
+
+def test_combine_edges():
+    assert combine_edges(EDGES[0], EDGES[1]) == (
+        "A",
+        "B",
+        {"weight": 2, "type": 1, "divisions": (1, 2)},
+    )
+    assert combine_edges(EDGES[5], EDGES[5]) == (
+        "A",
+        "B",
+        {"weight": 2, "type": None, "divisions": tuple()},
+    )

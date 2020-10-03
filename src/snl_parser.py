f9d400b1d5fff432bcaae8890bab83aedd6a7a5b
Cody VanZandt <codyvanzandt@wustl.edu>
2020-10-03 17:06:57 +0000

    Have basic parsing and serializing working!

--- src/snl_parser.py
+++ src/snl_parser.py
@@ -1,106 +1,103 @@
 from lark import Lark, Transformer
-from pprint import pprint
+from collections import ChainMap
 
 
 class TreeToDict(Transformer):
+    def space(self, children):
+        return " "
     def number(self, children):
         (n,) = children
         return float(n)
+    def name(self, children):
+        return "".join(children)
     def true(self, children):
         return True
     def false(self, children):
         return False
-    def space(self, children):
-        return " "
-    def CNAME(self, children):
-        return str(children)
-    def edge(self, children):
-        return "-".join(children)
-    def words(self, children):
-        return "".join(children)
+    def list(self, children):
+        return list(children)
     def dict(self, children):
         return dict(children)
     def pair(self, children):
         return tuple(children)
-    def dict_pair(self, children):
-        return tuple(children)
-    def section_symbol(self, children):
-        return "#"
-    def section_marker(self, children):
-        return "".join(children)
-    def section_header(self, children):
-        return " ".join(children)
-    # def section(self, children):
-    #     section_name, *section_data = children
-    #     return {section_name : dict(section_data)}
-    # def document(self, children):
+    def directed_edge(self, children):
+        return "".join( ("-",) + tuple(children) + (">",) )
+    def undirected_edge(self, children):
+        return "".join( ("-",) + tuple(children) + ("-",) )
+    def edge(self, children):
+        num_children = len(children)
+        if num_children == 3:
+            return tuple(children) + (dict(),)
+        elif num_children == 4:
+            return tuple(children)
+        else:
+            raise ValueError(f"edge {children} improperly formatted")
+    def section(self, children):
+        section_name, *section_data = children
+        if section_name == "edges":
+            return { section_name : tuple(section_data) }
+        else:
+            return { section_name : dict(section_data) }
+    def document(self, children):
+        return dict(ChainMap(*children))
+
+        
         
 
 snl_parser = Lark(r"""
-    ?value: map
+    ?value: dict
+      | list
+      | pair
       | number
       | name
       | true
       | false
 
+    space : (" " | /\t/)+
+
     number : SIGNED_NUMBER
-    name : CNAME (space+ CNAME)*
+    name : CNAME (space CNAME)*
     true : "true"
     false : "false"
 
-    space: " "
-    doublespace : "  "
-    newline : /\n/
+    list : "[" [value ("," value)*] "]"
+    dict : "{" [pair ("," pair)*] "}"
+    pair : name ":" value
+
+    edge : name (directed_edge | undirected_edge) name [":" value]
+    directed_edge : "-" name? ">"
+    undirected_edge : "-" name? "-"
 
     document: section*
-    section : name newline doublespace
-    map : name space? ":" space? value newline?
+    section : "#" name ( pair* | edge*)
 
     %import common.CNAME
     %import common.SIGNED_NUMBER
+    %import common.WS
+    %ignore WS
 
     """, start='document')
 
-doc = r"""play
-  title : Some Title
-  author : Some Author
+doc = r"""# play
+title : Some Title
+author : Some Author
 
-characters
-  Isabella
-    gender : female
-    archetype : innamorati
-  Flavio 
-    gender : male
-    archetype : innamorati 
-  Pantalone
-    gender : male
-    archetype : vecchi
+# node definitions
+Isabella : {gender : female, archetype : innamorati}
+Flavio : {gender : male, archetype : innamorati}
+Pantalone : {gender : male, archetype : vecchi }
 
-edges
-  act1
-    scene1
-      Isabella-Pantalone
-        weight : 1
-        type : hit
-      Isabella-Flavio
-        weight : 2
-        type : kissed 
-      Flavio-Pantalone
-        weight : 3
-        type : hit  
-    scene1
-      Isabella-Pantalone
-        weight : 1
-        type : hit
-      Isabella-Flavio
-        weight : 2
-        type : kissed 
-      Flavio-Pantalone
-        weight : 3
-        type : hit
+# edge definitions
+KISSED : {type : kissed}
+HIT : {type : hit}
+
+
+# edges
+Isabella -> Flavio : { type : kissed }
+Isabella -- Pantalone : { type : hit }
+Isabella -KISSED> Flavio
+Flavio -HIT- Pantalone
 """
-print(repr(doc))
 
 tree = snl_parser.parse(doc)
-print(tree.pretty())
-# pprint( TreeToDict().transform(tree) )
+print( TreeToDict().transform(tree) )
